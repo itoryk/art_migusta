070c0fbb6fb71ad83e88df6f5837b049c2d715e7
Viktoriya <tory-tory-00@mail.ru>
2021-02-09 23:23:42 +0300

    include static

--- art_migusta/urls.py
+++ art_migusta/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from landing.views import IndexView
+from application.landing.views import IndexView
 
 
 urlpatterns = [
