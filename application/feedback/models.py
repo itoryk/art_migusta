8cc205f19deb78abb50c561293e36952dd66de1b
Viktoriya <tory-tory-00@mail.ru>
2021-02-14 13:57:26 +0300

    db

--- application/feedback/models.py
+++ application/feedback/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class FeedbackModel(models.Model):
+    name = models.CharField(max_length=30)
+    comment = models.CharField(max_length=400)
--- application/feedback/views.py
+++ application/feedback/views.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.views.generic import FormView
-from framework.mixins import ExtendedContextMixin
+from application.feedback.models import FeedbackModel
+from django.forms import ModelForm, CharField
 
 
 class FeedbackForm(forms.Form):
@@ -12,13 +13,22 @@ class FeedbackForm(forms.Form):
         pass
 
 
+class FeedbackForm2(ModelForm):
+    '''name = forms.CharField(required=False)
+    comment = forms.CharField(required=False)
+    model = FeedbackModel'''
+    class Meta:
+        model = FeedbackModel
+        fields = ['name', 'comment']
+
+
 class FeedbackView(FormView):
-    form_class = FeedbackForm
+    form_class = FeedbackForm2
     success_url = "/feedback/"
     template_name = "feedback/index.html"
 
     def form_valid(self, form):
-        form.insert_comment()
+        form.save()
         return super().form_valid(form)
 
 
