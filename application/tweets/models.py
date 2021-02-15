2544bb62f2950a50167ea17d6d3678ba8ed9b9de
Viktoriya <tory-tory-00@mail.ru>
2021-02-15 12:45:35 +0300

    adminka

---
+++ application/tweets/models.py
@@ -0,0 +1,13 @@
+from django.contrib.contenttypes.fields import GenericRelation
+from django.db import models
+from application.likes.models import Like
+
+
+class Tweet(models.Model):
+    body = models.CharField(max_length=140)
+    likes = GenericRelation(Like)
+    def __str__(self):
+        return self.body
+    @property
+    def total_likes(self):
+        return self.likes.count()
